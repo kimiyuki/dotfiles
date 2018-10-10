5d8950f8cccd519580ac223440d0713718a3350f
kimiyuki shirai <kimiyuki@gmail.com>
2018-10-10 12:07:28 +0900

    add

--- 00-first.py
+++ 00-first.py
@@ -1,4 +1,11 @@
 get_ipython().run_line_magic('load_ext', 'autoreload')
 get_ipython().run_line_magic('autoreload', '2')
+get_ipython().run_line_magic('xmode', 'Plain')
 print('autoreload loaded from startup ~/.ipython/...')
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+print('plt, np, pd, sns are loaded')
+
 
