befde0a1844f509949035ce1346f0c59926c6a63
kimiyuki shirai <kimiyuki@gmail.com>
2018-10-11 20:01:16 +0900

    enhancing

--- 00-ipython-startup.py
+++ 00-ipython-startup.py
@@ -5,7 +5,17 @@ print('autoreload loaded from startup ~/.ipython/...')
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from pandas_datareader import data
+pd.set_option("display.max_rows", 6)
 import seaborn as sns
+iris = sns.load_dataset("iris")
+mpg = sns.load_dataset("mpg")
+flights = sns.load_dataset("flights")
+diamonds = sns.load_dataset("diamonds")
+titanic = sns.load_dataset("titanic")
+planets = sns.load_dataset("planets")
 print('plt, np, pd, sns are loaded')
+import requests
+from bs4 import BeautifulSoup
 
 
