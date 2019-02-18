afca8ba6e02c85cb9e302aa4746269304d7f51ea
kimiyuki shirai <kimiyuki@gmail.com>
2019-02-18 09:17:21 +0900

    fix_some.sh why

--- fix_some.sh
+++ fix_some.sh
@@ -1,5 +1,13 @@
-#syndaemon -i 0.5 -R -d
-if [ $HOSTNAME == 'mb2' ]; then
+sudo rmmod i2c_hid
+sudo modprobe i2c_hid
+echo "$HOSTNAME"
+# use "_" by シェルスクリプトシンプルレシピ
+if [ "_$HOSTNAME" = "_mb2" ]; then
+  echo 'start'
+  echo "does not work properly, why?"
+  syndaemon -i 0.3 -R -d
   synclient AreaLeftEdge=100 AreaRightEdge=1200
   synclient AreaTopEdge=50 AreaBottomEdge=800
+  result=$(synclient -l|grep area.*edge)
+  echo "$result"
 fi
